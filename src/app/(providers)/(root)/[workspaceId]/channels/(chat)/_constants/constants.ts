5a6aaf3c90528ba643de8c6af10b4ed3331c2d09
yerin <dev.yerin@gmail.com>
2024-09-16 15:57:28 +0900

    refactor: query keys type 상수화

--- src/app/(providers)/(root)/[workspaceId]/channels/(chat)/_constants/constants.ts
+++ src/app/(providers)/(root)/[workspaceId]/channels/(chat)/_constants/constants.ts
@@ -1,11 +1,11 @@
 export const QUERY_KEYS = {
-  CHAT_MESSAGES: (channel_id: number) => ['chatMessages', channel_id],
-  USERS_IN_CHANNEL: (channel_id: number) => ['usersInChannel', channel_id],
-  CHANNEL_INFO: (id: number) => ['channelInfo', id],
-  LATEST_NOTICE: (id: number) => ['latestNotice', id],
-  CHANNEL_DOCUMENTS: (id: number) => ['channelDocuments', id],
-  CHANNEL_MEDIA: (id: number) => ['channelMedia', id],
-  CHANNEL_NOTICES: (id: number) => ['channelNotices', id]
+  CHAT_MESSAGES: (channel_id: number) => ['chatMessages', channel_id] as const,
+  USERS_IN_CHANNEL: (channel_id: number) => ['usersInChannel', channel_id] as const,
+  CHANNEL_INFO: (id: number) => ['channelInfo', id] as const,
+  LATEST_NOTICE: (id: number) => ['latestNotice', id] as const,
+  CHANNEL_DOCUMENTS: (id: number) => ['channelDocuments', id] as const,
+  CHANNEL_MEDIA: (id: number) => ['channelMedia', id] as const,
+  CHANNEL_NOTICES: (id: number) => ['channelNotices', id] as const
 };
 
 export const STORAGE_BUCKET_NAME = {
--- src/app/(providers)/(root)/[workspaceId]/channels/_constants/constants.ts
+++ src/app/(providers)/(root)/[workspaceId]/channels/_constants/constants.ts
@@ -1,5 +1,5 @@
 export const QUERY_KEYS = {
-  CHANNELS: (workspaceId: number | null) => ['channels', workspaceId],
-  CHANNEL_ID: (workspaceId: number | null) => ['channelId', workspaceId],
-  SEARCH_WORKSPACE_USERS: (workspaceId: number) => ['searchWorkspaceUsers', workspaceId]
+  CHANNELS: (workspaceId: number | null) => ['channels', workspaceId] as const,
+  CHANNEL_ID: (workspaceId: number | null) => ['channelId', workspaceId] as const,
+  SEARCH_WORKSPACE_USERS: (workspaceId: number) => ['searchWorkspaceUsers', workspaceId] as const
 };
